e8ed1749617bfcdb99d279dc60381cc1ba19c8a2
karman103 <karmanbir103@gamil.com>
2023-08-01 17:52:17 +0530

    Adding Clerk files

--- app/(landing)/page.tsx
+++ app/(landing)/page.tsx
@@ -1,7 +1,22 @@
+import { Button } from "@/components/ui/button";
+import Link from "next/link";
+
 const Landingpage =() => {
     return (
         <div> 
             Landing Page (Unprotecetd)
+            <div>
+                <Link href="/sign-in">
+                    <Button>
+                        Sign-in
+                    </Button>
+                </Link>
+                <Link href="/sign-up">
+                    <Button>
+                        Sign-up
+                    </Button>
+                </Link>
+            </div>
         </div>
     );
 }
