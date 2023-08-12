a3ca77d21e17ecd74621cc59cec3d8a8bf2039fd
karman103 <karmanbir103@gamil.com>
2023-08-12 12:17:27 +0530

    fixed minor bug of coveration route instead of conversation

--- components/sidebar.tsx
+++ components/sidebar.tsx
@@ -31,7 +31,7 @@ const routes = [
     {
         label:"Conversation",
         icon: MessageSquare,
-        href:"/converation",
+        href:"/conversation",
         color: "text-violet-500"
     },
     {
